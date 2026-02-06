d42b92989507bfcc0dccdc4d291ab31899bd20ba
vchhavi68 <vchhavi68@gmail.com>
2026-02-06 20:04:06 +0530

    Add Menu component to display menu items

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,1 +1,31 @@
 
+import React from "react"
+
+const Menu = ({items}) => {
+    return(
+        <div className="section-center">
+            {
+                items.map((menuItem)=>{
+
+                    const {id,title,img,desc,price, category} = menuItem;
+
+                    return(
+                        <article category={category} key={id} className="menu-item " data-test-id={`menu-item-${category}`} >
+                            <img src={img} alt={title} className="photo" />
+                            <div className="item-info">
+                                <header>
+                                    <h4>{title}</h4>
+                                    <h4 className="price">$ {price}</h4>
+                                    
+                                </header>
+                                <p className="item-text">{desc}</p>
+                            </div>
+                        </article>
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+export default Menu
