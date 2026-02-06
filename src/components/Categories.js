a1f594ca088cddb3f69d8be55e2aebf97b0ed98a
vchhavi68 <vchhavi68@gmail.com>
2026-02-06 20:03:30 +0530

    Add Categories component to filter items

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,1 +1,26 @@
 
+import React from "react"
+
+
+const Categories = ({categories,filterItems}) => {
+
+    return(
+        <div className="btn-container">
+            {
+                categories.map((category,index)=>{
+                    return(
+                        <button id={`filter-btn-${index}`}
+                            type="button"
+                            className="filter-btn"
+                            key={index}
+                            onClick={()=>filterItems(category)}>
+                                {category}
+                        </button>
+                    )
+                })
+            }    
+        </div>
+    )
+}
+
+export default Categories
